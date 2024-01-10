505eb0a5bf981d6c8447b82efbbea9e7dee5ba2e
martalionaite <153713084+martalionaite@users.noreply.github.com>
2024-01-10 10:01:41 +0200

    array

--- uzdaviniai/biggest.js
+++ uzdaviniai/biggest.js
@@ -2,9 +2,12 @@ function biggest (list){
    if (!Array.isArray(list)) {
     return 'ERROR: duok array tipo reiksme';
    }
-let maxNumber = 0;
+let maxNumber = list[0];
 
-console.log(list);
+for (let i = 0; i<list.length; i++) {
+   if (list[i] > maxNumber) {
+      maxNumber = list[i];   }
+}
 
    return maxNumber;
     
@@ -28,5 +31,9 @@ console.log(biggest([1]), '---->', 1);
 console.log(biggest([1, 2, 3,]), '---->', 3);
 console.log(biggest([4, 5, 1, 7]), '---->', 7);
 console.log(biggest([-5, 78, 14, 0, 18]), '---->', 78);
-//console.log(biggest([69, 69, 69, 69, 66]), '---->', 69);
-//console.log(biggest([-1, -2, -3, -4, -5, -6, -7, -8]), '---->', -1);
+console.log(biggest([69, 69, 69, 69, 66]), '---->', 69);
+console.log(biggest([-1, -2, -3, -4, -5, -6, -7, -8]), '---->', -1);
+console.log(biggest([-11, -22, -23, -44, -55, -66,]), '---->', -11);
+
+console.log(biggest[4, 5, 1, 7, 'labas','----->', 7]);
+console.log(biggest[4, 5, 1, 7, 'labas','----->', 7]);
